170f5828e4ead46ea5d3622264101488dc97e1fe
Gor Madatyan <gor-madatyan@proton.me>
2026-04-12 23:35:31 +0400

    Add basic Data types

---
+++ src/protocol.rs
@@ -0,0 +1,20 @@
+use bytes::Bytes;
+type RecursiveData = Box<Data>;
+
+/// Represents all the data that is sent __and/or__ received by server
+/// ## BY THE WAY
+/// you can store `JSON` and/or other serialization formats as ByteStream or String. It is up to you
+pub enum Data {
+    /// Used for sending raw bytes (there is not better type for them), e.g. images
+    ByteStream(Bytes),
+    /// UTF-8 encoded string
+    String(String),
+    /// Signed 64 bits integer
+    SInteger(i64),
+    /// Unsigned 64 bits integer
+    UInteger(u64),
+    /// Array of [`Data`]
+    Array(RecursiveData),
+    /// Used for such purposes as to ping a server
+    NULL
+}
