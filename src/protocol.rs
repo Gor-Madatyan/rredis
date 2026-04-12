34e373fb42500c3810ed6ac038b5d2c2752422a8
Gor Madatyan <gor-madatyan@proton.me>
2026-04-13 00:07:18 +0400

    Add Frame

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,5 +1,5 @@
 use bytes::Bytes;
-type RecursiveData = Box<Data>;
+type ManyData = Vec<Data>;
 
 /// Represents all the data that is sent __and/or__ received by server
 /// ## BY THE WAY
@@ -14,7 +14,17 @@ pub enum Data {
     /// Unsigned 64 bits integer
     UInteger(u64),
     /// Array of [`Data`]
-    Array(RecursiveData),
-    /// Used for such purposes as to ping a server
+    Array(ManyData),
+    /// Used to unset fields in set requests
     NULL
+}
+
+/// The basic unit transferred over network
+/// `key` is used to specify 'path'\
+/// if the `data` is [`None`], it is implied that it is a get request\
+/// `payload` is used to set additional context
+pub struct Frame<T:ToString> {
+    key: T,
+    data: Option<Data>,
+    payload: Option<Data>
 }
