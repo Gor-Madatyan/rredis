a003b765a6e7dc5adcfb54d5d49851bf10497380
Gor Madatyan <gor-madatyan@proton.me>
2026-04-14 20:56:56 +0400

    improve Frame/Request interface

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,15 +1,10 @@
 use bytes::Bytes;
 pub type ManyData = Vec<Data>;
 
-/// The request types that can be sent/recieved
-pub enum RequestType{
-    Get,
-    Set
-}
-
 /// Represents all the data that is sent __and/or__ received by server
 /// ## BY THE WAY
 /// you can store `JSON` and/or other serialization formats as ByteStream or String. It is up to you
+#[derive(Clone, Debug)]
 pub enum Data {
     /// Used for sending raw bytes (there is not better type for them), e.g. images
     ByteStream(Bytes),
@@ -25,42 +20,57 @@ pub enum Data {
     NULL
 }
 
+/// The request types that can be sent/recieved
+#[derive(Clone, Debug)]
+pub enum Request<T:ToString>{
+    /// Get request, __sent to server__
+    Get{key:T},
+    /// Set request, __sent to server__
+    Set{key:T,value:Data},
+    /// plain data, __sent to client__
+    Data{value:Data},
+}
+
 /// The basic unit transferred over network.\
-/// get and set requests are both implemented as the same [`Frame`] structure.
+/// it encapsulates all the possible means of communication acceptable.\
+/// everything transferred over the network is a Frame.
 /// # See also
-/// [`Frame::new_set_request`] and [`Frame::new_get_request`]
+/// [`Frame::new_data_request`], [`Frame::new_set_request`] and [`Frame::new_get_request`]
+#[derive(Clone, Debug)]
 pub struct Frame<T:ToString> {
-    // the key to access
-    key: T,
-    // data to transfer (optional for get requests)
-    data: Option<Data>,
+    // the core request
+    request: Request<T>,
     // additional context
     payload: Option<Data>
 }
 
 impl<T> Frame<T> where T: ToString{
-
-    /// create new `get` request\
-    /// since the request is get, only the key to get is required and optional payload
+    /// create new `get` request
     pub fn new_get_request(key: T, payload: Option<Data>) -> Self {
         Self{
-            key,
-            data:None,
+            request: Request::Get{key},
             payload
         }
     }
 
-    /// create new `get` request\
-    /// since the request is get, only the key to get is required and optional payload
-    pub fn new_set_request(key: T, data: Data, payload: Data) -> Self {
+    /// create new `set` request
+    pub fn new_set_request(key: T, data: Data, payload: Option<Data>) -> Self {
         Self{
-            key,
-            data:Some(data),
-            payload:Some(payload)
+            request:Request::Set{key,value:data},
+            payload
         }
     }
 
-    pub fn request_type(&self) -> RequestType {
-        if let Some(_) = self.data { RequestType::Set } else { RequestType::Get }
+    /// create [`Frame`] to respond to a request made by a client
+    pub fn new_data_request(data: Data, payload: Option<Data>) -> Self {
+        Self{
+            request:Request::Data{value:data},
+            payload
+        }
+    }
+
+    /// gives the primary request and the optional payload
+    pub fn decompose(self) -> (Request<T>, Option<Data>) {
+        (self.request, self.payload)
     }
 }
