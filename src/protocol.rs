958eb3d118ee49435592df57bf0b66e30374fde9
Gor Madatyan <gor-madatyan@proton.me>
2026-05-01 23:18:11 +0400

    add the trait implementations for converting user structures to proto structures

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,6 +1,7 @@
-use std::error::Error;
-use std::fmt::{Debug};
+use crate::repr;
 use bytes::Bytes;
+use std::error::Error;
+use std::fmt::{Debug, Display};
 pub type ManyData = Vec<Data>;
 
 /// Represents all the data that is sent __and/or__ received by server
@@ -19,20 +20,21 @@ pub enum Data {
     /// Array of [`Data`]
     Array(ManyData),
     /// Used to unset fields in set requests
-    NULL
+    NULL,
 }
 
 /// The request types that can be sent/recieved
 #[derive(Clone, Debug)]
 pub enum Request<T>
-    where T:Into<String>
+where
+    T: Into<String>,
 {
     /// Get request, __sent to server__
-    Get{key:T},
+    Get { key: T },
     /// Set request, __sent to server__
-    Set{key:T,value:Data},
+    Set { key: T, value: Data },
     /// plain data, __sent to client__
-    Data{value:Data},
+    Data { value: Data },
 }
 
 /// The basic unit transferred over network.\
@@ -41,41 +43,41 @@ pub enum Request<T>
 /// # See also
 /// [`Frame::new_data_request`], [`Frame::new_set_request`] and [`Frame::new_get_request`]
 #[derive(Clone, Debug)]
-pub struct Frame<T:Into<String>> {
+pub struct Frame<T: Into<String>> {
     // the core request
     request: Request<T>,
     // additional context
-    payload: Option<Data>
+    payload: Option<Data>,
 }
 
-impl<T> Frame<T> where T: Into<String>{
-    pub(crate) fn new(request: Request<T>, payload:Option<Data>) -> Frame<T> {
-        Self{
-            request,
-            payload
-        }
+impl<T> Frame<T>
+where
+    T: Into<String>,
+{
+    pub(crate) fn new(request: Request<T>, payload: Option<Data>) -> Frame<T> {
+        Self { request, payload }
     }
     /// create new `get` request
     pub fn new_get_request(key: T, payload: Option<Data>) -> Self {
-        Self{
-            request: Request::Get{key},
-            payload
+        Self {
+            request: Request::Get { key },
+            payload,
         }
     }
 
     /// create new `set` request
     pub fn new_set_request(key: T, data: Data, payload: Option<Data>) -> Self {
-        Self{
-            request:Request::Set{key,value:data},
-            payload
+        Self {
+            request: Request::Set { key, value: data },
+            payload,
         }
     }
 
     /// create [`Frame`] to respond to a request made by a client
     pub fn new_data_request(data: Data, payload: Option<Data>) -> Self {
-        Self{
-            request:Request::Data{value:data},
-            payload
+        Self {
+            request: Request::Data { value: data },
+            payload,
         }
     }
 
@@ -86,25 +88,80 @@ impl<T> Frame<T> where T: Into<String>{
 }
 
 #[derive(Debug)]
-pub struct RRError{
+pub struct RRError {
     error: Option<Box<dyn Error>>,
-    message:String
+    message: String,
 }
 
-impl<E:Error + 'static> From<E> for RRError{
+impl<E: Error + 'static> From<E> for RRError {
     fn from(value: E) -> Self {
         let message = value.to_string();
-        Self{error:Some(Box::new(value)),message }
+        Self {
+            error: Some(Box::new(value)),
+            message,
+        }
     }
 }
 
 impl RRError {
-    pub fn new(message:impl Into<String>) -> RRError{
-        Self{error:None, message:message.into()}
+    pub fn new(message: impl Into<String>) -> RRError {
+        Self {
+            error: None,
+            message: message.into(),
+        }
     }
     pub fn source(&self) -> Option<&(dyn Error + 'static)> {
-       if let Some(ref err) = self.error {
-           Some(&**err)
-       }else { None }
+        if let Some(ref err) = self.error {
+            Some(&**err)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T:Into<String> + Display> From<Frame<T>> for repr::Frame {
+    fn from(value: Frame<T>) -> Self {
+         Self{
+             payload: value.payload.map(|d|d.into()),
+             request:Some(value.request.into()),
+
+        }
+    }
+}
+
+impl<T:Into<String>> From<Request<T>> for repr::frame::Request{
+    fn from(value: Request<T>) -> Self {
+        match value {
+            Request::Get { key } => repr::frame::Request::Get(repr::GetRequest{key:key.into()}),
+            Request::Set { key,value } => repr::frame::Request::Set(repr::SetRequest{key:key.into(), value: Some(value.into())}),
+            Request::Data { value } => repr::frame::Request::Data(repr::DataRequest{value:Some(value.into())}),
+        }
+    }
+}
+
+impl From<Data> for repr::Data {
+    fn from(value: Data) -> Self {
+        match value {
+            Data::UInteger(d) => repr::Data {
+                kind: Some(repr::data::Kind::UInteger(d)),
+            },
+            Data::SInteger(d) => repr::Data {
+                kind: Some(repr::data::Kind::SInteger(d)),
+            },
+            Data::String(s) => repr::Data {
+                kind: Some(repr::data::Kind::StringValue(s)),
+            },
+            Data::NULL => repr::Data {
+                kind: Some(repr::data::Kind::NullValue(repr::NullValue {})),
+            },
+            Data::ByteStream(b) => repr::Data {
+                kind: Some(repr::data::Kind::ByteStream(b.into())),
+            },
+            Data::Array(a) => repr::Data {
+                kind: Some(repr::data::Kind::Array(repr::ManyData {
+                    elements: a.into_iter().map(|d| d.into()).collect(),
+                })),
+            },
+        }
     }
 }
