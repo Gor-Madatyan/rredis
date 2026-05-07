39740acfdcb3a03a7b117343b60253749dbb2534
Gor Madatyan <gor-madatyan@proton.me>
2026-05-07 19:07:04 +0400

    changed the From implementations to TryFrom

--- src/connection.rs
+++ src/connection.rs
@@ -1,6 +1,6 @@
 pub mod server;
 use crate::protocol::error::{NetworkErrorKind, RRErrorKind, SerializationErrorKind};
-use crate::protocol::{error::RRError, Frame, NetworkFrame};
+use crate::protocol::{Frame, NetworkFrame, error::RRError};
 use crate::repr;
 use prost::Message;
 use std::fmt::Display;
@@ -9,11 +9,11 @@ use tokio::net::{TcpStream, ToSocketAddrs};
 
 /// The main structure you will work with.
 /// You will send [Frame]s and/or receive them through this abstraction.
-pub struct Connection{
-    socket:TcpStream
+pub struct Connection {
+    socket: TcpStream,
 }
 
-impl Connection{
+impl Connection {
     pub fn new(socket: TcpStream) -> Self {
         Self { socket }
     }
@@ -21,57 +21,66 @@ impl Connection{
     /// encapsulate the [TcpStream]
     pub async fn to(socket: impl ToSocketAddrs) -> Result<Self, RRError> {
         Ok(Self {
-            socket: TcpStream::connect(socket).await.map_err(|_| RRErrorKind::NetworkError(
-                NetworkErrorKind::ConnectionFailed,
-            ))?
+            socket: TcpStream::connect(socket)
+                .await
+                .map_err(|_| RRErrorKind::NetworkError(NetworkErrorKind::ConnectionFailed))?,
         })
     }
 
     /// Abstraction for sending [Frame] and awaiting the response
-    pub async fn sendrecv(&mut self, frame: Frame<impl Into<String> + Display>) -> Result<NetworkFrame, RRError> {
+    pub async fn sendrecv(
+        &mut self,
+        frame: Frame<impl Into<String> + Display>,
+    ) -> Result<NetworkFrame, RRError> {
         self.write_frame(frame).await?;
         self.read_frame().await
     }
 
     /// Send the [Frame].
     /// When the connection closes you will receive an [error](RRError)
-    pub async fn write_frame<T:Into<String> + Display>(&mut self, frame: Frame<T>)->Result<(),RRError>{
-        let frame:repr::Frame = frame.into();
+    pub async fn write_frame<T: Into<String> + Display>(
+        &mut self,
+        frame: Frame<T>,
+    ) -> Result<(), RRError> {
+        let frame: repr::Frame = frame.into();
         let b = frame.encode_length_delimited_to_vec();
-        self.socket.write_all(b.as_slice()).await.map_err(|_| RRErrorKind::NetworkError(
-            NetworkErrorKind::FrameWriteError,
-        ))?;
+        self.socket
+            .write_all(b.as_slice())
+            .await
+            .map_err(|_| RRErrorKind::NetworkError(NetworkErrorKind::FrameWriteError))?;
         Ok(())
     }
 
     /// Suspend until the next [Frame] comes.
     /// When the connection closes you will receive an [error](RRError)
     pub async fn read_frame(&mut self) -> Result<NetworkFrame, RRError>
-    where
-    {
+where {
         let len = self.advance_stream().await?;
         let mut buf = vec![0u8; len];
-        self.socket.read_exact(&mut buf).await.map_err(|_| RRErrorKind::NetworkError(
-            NetworkErrorKind::FrameReadError,
-        ))?;
-        Ok(Result::from(repr::Frame::decode(buf.as_slice()).map_err(|_| RRErrorKind::SerializationError(
-            SerializationErrorKind::FormatError
-        ))?)?)
+        self.socket
+            .read_exact(&mut buf)
+            .await
+            .map_err(|_| RRErrorKind::NetworkError(NetworkErrorKind::FrameReadError))?;
+        Ok(Frame::try_from(
+            repr::Frame::decode(buf.as_slice()).map_err(|_| {
+                RRErrorKind::SerializationError(SerializationErrorKind::FormatError)
+            })?,
+        )?)
     }
 
     /// Advances the stream past the length delimiter and returns the delimiter so you can continue reading
-    async fn advance_stream(&mut self)->Result<usize,RRError>{
-        let mut buf = [0u8;8];
-        for i in 0..10{
-            buf[i] = self.socket.read_u8().await.map_err(|_| RRErrorKind::NetworkError(
-                NetworkErrorKind::FrameReadError,
-            ))?;
-            if let Ok(len) = prost::decode_length_delimiter(buf.as_slice()){
+    async fn advance_stream(&mut self) -> Result<usize, RRError> {
+        let mut buf = [0u8; 8];
+        for i in 0..10 {
+            buf[i] = self
+                .socket
+                .read_u8()
+                .await
+                .map_err(|_| RRErrorKind::NetworkError(NetworkErrorKind::FrameReadError))?;
+            if let Ok(len) = prost::decode_length_delimiter(buf.as_slice()) {
                 return Ok(len);
             }
         }
-        Err(RRErrorKind::SerializationError(
-            SerializationErrorKind::FormatError
-        ).into())
+        Err(RRErrorKind::SerializationError(SerializationErrorKind::FormatError).into())
     }
 }
--- src/repr.rs
+++ src/repr.rs
@@ -1,7 +1,9 @@
 mod error_conversions;
 
 use crate::protocol;
-use crate::protocol::error::{RRError, RRErrorKind, SerializationErrorKind as PSerializationErrorKind};
+use crate::protocol::error::{
+    RRError, RRErrorKind, SerializationErrorKind as PSerializationErrorKind,
+};
 use crate::repr::frame::Request;
 
 // to include codegen
@@ -9,61 +11,62 @@ include!(concat!(env!("OUT_DIR"), "/network_protocol.rs"));
 
 // Conversions to protocol types
 
-impl From<Frame> for Result<protocol::NetworkFrame, RRError> {
-    fn from(value: Frame) -> Self {
+impl TryFrom<Frame> for protocol::NetworkFrame {
+    type Error = RRError;
+    fn try_from(value: Frame) -> Result<Self, Self::Error> {
         let request = value.request;
         let payload = value.payload;
 
-        let request = Result::from(request.ok_or(RRErrorKind::SerializationError(
-            PSerializationErrorKind::FieldNotOptional("request".into()),
-        ))?)?;
+        let request =
+            protocol::Request::try_from(request.ok_or(RRErrorKind::SerializationError(
+                PSerializationErrorKind::FieldNotOptional("request".into()),
+            ))?)?;
         let payload = match payload {
             None => None,
-            Some(payload) => Some(Result::from(payload)?),
+            Some(payload) => Some(protocol::Data::try_from(payload)?),
         };
         Ok(protocol::Frame::new(request, payload))
     }
 }
 
-impl From<Request> for Result<protocol::Request<String>, RRError> {
-    fn from(value: Request) -> Self {
+impl TryFrom<Request> for protocol::Request<String> {
+    type Error = RRError;
+    fn try_from(value: Request) -> Result<protocol::Request<String>, RRError> {
         match value {
             Request::Get(req) => Ok(protocol::Request::Get { key: req.key }),
             Request::Set(req) => Ok(protocol::Request::Set {
                 key: req.key,
-                value: Result::from(req.value.ok_or(RRErrorKind::SerializationError(
-                    PSerializationErrorKind::FieldNotOptional("value (set request)".into()),
-                ))?)?,
+                value: protocol::Data::try_from(req.value.ok_or(
+                    RRErrorKind::SerializationError(PSerializationErrorKind::FieldNotOptional(
+                        "value (set request)".into(),
+                    )),
+                )?)?,
             }),
             Request::Data(req) => Ok(protocol::Request::Data {
-                value: Result::from(req.value.ok_or(RRErrorKind::SerializationError(
-                    PSerializationErrorKind::FieldNotOptional("value (data request)".into()),
-                ))?)?,
+                value: protocol::Data::try_from(req.value.ok_or(
+                    RRErrorKind::SerializationError(PSerializationErrorKind::FieldNotOptional(
+                        "value (data request)".into(),
+                    )),
+                )?)?,
             }),
-            Request::Error(e) => {
-                Ok(protocol::Request::Error {
-                    error: RRError::new(
-                        Result::from(e.kind.ok_or(
-                            RRErrorKind::SerializationError(
-                                PSerializationErrorKind::FieldNotOptional("kind (error)".into())
-                            )
-                        )?)?,
-                        e.message,
-                    )
-                })
-            }
+            Request::Error(e) => Ok(protocol::Request::Error {
+                error: RRError::new(
+                    Result::from(e.kind.ok_or(RRErrorKind::SerializationError(
+                        PSerializationErrorKind::FieldNotOptional("kind (error)".into()),
+                    ))?)?,
+                    e.message,
+                ),
+            }),
         }
     }
 }
 
-impl From<Data> for Result<protocol::Data, RRError> {
-    fn from(value: Data) -> Self {
-        match value
-            .kind
-            .ok_or(RRErrorKind::SerializationError(
-                PSerializationErrorKind::FieldNotOptional("kind (data.kind)".into()),
-            ))?
-        {
+impl TryFrom<Data> for protocol::Data {
+    type Error = RRError;
+    fn try_from(value: Data) -> Result<Self, Self::Error> {
+        match value.kind.ok_or(RRErrorKind::SerializationError(
+            PSerializationErrorKind::FieldNotOptional("kind (data.kind)".into()),
+        ))? {
             data::Kind::UInteger(int) => Ok(protocol::Data::UInteger(int)),
             data::Kind::SInteger(int) => Ok(protocol::Data::SInteger(int)),
             data::Kind::NullValue(_) => Ok(protocol::Data::NULL),
@@ -71,7 +74,7 @@ impl From<Data> for Result<protocol::Data, RRError> {
             data::Kind::Array(arr) => Ok(protocol::Data::Array({
                 let mut v: Vec<protocol::Data> = Vec::new();
                 for d in arr.elements {
-                    v.push(Result::from(d)?)
+                    v.push(protocol::Data::try_from(d)?)
                 }
                 v
             })),
